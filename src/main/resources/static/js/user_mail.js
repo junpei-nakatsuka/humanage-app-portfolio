cf26d6ecdd5fb1e187565e39b5ef1b40c182af69
junpei-nakatsuka <nft0722@gmail.com>
2025-02-12 11:44:20 +0900

    edit04

--- src/main/resources/static/js/user_mail.js
+++ src/main/resources/static/js/user_mail.js
@@ -1,14 +1,4 @@
-//function combineEmail(){let e=document.getElementById("email-local").value,l=document.getElementById("email-domain").value;document.getElementById("email").value=e+l}document.getElementById("phone").addEventListener("input",function(e){let l=e.target.value.replace(/[^0-9]/g,"");l.length>3&&l.length<=7?l=l.slice(0,3)+"-"+l.slice(3):l.length>7&&(l=l.slice(0,3)+"-"+l.slice(3,7)+"-"+l.slice(7)),e.target.value=l});
-
 window.onload = function() {
-    let phoneElement = document.getElementById("phone");
-    if (phoneElement) {
-        phoneElement.addEventListener("input", function(e) {
-            let l = e.target.value.replace(/[^0-9]/g, "");
-            l.length > 3 && l.length <= 7 ? l = l.slice(0, 3) + "-" + l.slice(3) : l.length > 7 && (l = l.slice(0, 3) + "-" + l.slice(3, 7) + "-" + l.slice(7));
-            e.target.value = l;
-        });
-    }
 
     let emailLocalElement = document.getElementById("email-local");
     let emailDomainElement = document.getElementById("email-domain");
